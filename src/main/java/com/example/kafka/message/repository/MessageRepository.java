9800d2fb614b63bf2ea8e1f299574d867e236379
John Ngai <jongai@blackberry.com>
2025-01-14 15:20:59 -0500

    Create a MessageRepository class

---
+++ src/main/java/com/example/kafka/message/repository/MessageRepository.java
@@ -0,0 +1,19 @@
+package com.example.kafka.message.repository;
+
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.ArrayList;
+
+@Component
+public class MessageRepository {
+
+    private List<String> list = new ArrayList<>();
+
+    public void addMessage(String message) {
+        list.add(message);
+    }
+
+    public String getAllMessages() {
+        return list.toString();
+    }
+}
